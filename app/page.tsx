d49fd07970553aa1f2920a0b6cbbc0b84982717e
ntpxxz <63597391+ntpxxz@users.noreply.github.com>
2024-05-29 17:29:47 +0700

    2 commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import General from "@/components/Cards/General";
+export default function Home() {
+  return (
+ 
+      <div>
+        <General></General>
+       <Button variant="outline">Export</Button>
+      </div>
+  
+  );
+}
